30315c4ed12b3d0f371f251fe22c3ecc8a2dd429
Travis Gee <geet084@gmail.com>
2019-04-16 07:45:40 -0600

    Adds display component

---
+++ src/components/Display.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Nasa APoD</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Display',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1 {
+  margin: 40px 0 0;
+}
+</style>
