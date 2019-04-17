7cb5ec2795b78a32a695aaa34ab7c39b44a2d823
Travis Gee <geet084@gmail.com>
2019-04-17 08:40:41 -0600

    Adds archive component for MTD thumbnails

---
+++ src/components/Archive.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h3>Archives</h3>
+    <div class="thumb">
+      <div v-for="archive in archives" class="pic">
+        <img :src="archive.url" alt>
+        {{archive.date}}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Archive",
+  props: {
+    archives: Array
+  }
+};
+</script>
+
+<style scope>
+.thumb {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+.thumb img {
+  max-width: 200px;
+}
+.pic {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 10px;
+}
+</style>
