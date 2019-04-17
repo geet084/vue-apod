8aaccb86838169ea1c50aeded6c4f47448785375
Travis Gee <geet084@gmail.com>
2019-04-17 08:41:28 -0600

    Adds fetch calls for individual images and thumbnail gallery

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,75 @@
 <template>
   <div id="app">
-    <Display :apodImage="this.apodImage"/>
+    <Nav :toggleArchive="this.toggleArchive"/>
+    <Archive v-if="this.showArchives" :archives="this.archives"/>
+    <Display v-else :apodImage="this.apodImage"/>
   </div>
 </template>
 
 <script>
-import Display from './components/Display.vue'
+import Display from "./components/Display.vue";
+import Nav from "./components/Nav.vue";
+import Archive from "./components/Archive.vue";
 
 export default {
-  name: 'app',
-  created: function () {
-    this.fetchPic()
+  name: "app",
+  created: function() {
+    this.fetchPic(), this.fetchAllPics(), this.toggleArchive();
   },
-  data () {
+  data() {
     return {
-      apodImage: {}
-    }
+      apodImage: {},
+      archives: [],
+      showArchives: false
+    };
   },
   components: {
-    Display
+    Display,
+    Nav,
+    Archive
   },
   methods: {
-    fetchPic: function () {
+    fetchPic: function() {
       const root = 'https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY'
-      this.$http.get(root)
-      .then(result => {
-        console.log(result.data)
-        this.apodImage = result.data
-      })
+      
+      this.$http.get(root).then(result => {
+        this.apodImage = result.data;
+      });
+    },
+    fetchAllPics: function() {
+      let newDate = new Date();
+      let date = newDate.toDateString().split(" ");
+      let months = ["none", "Jan", "Feb", "Mar", "Apr", "May"];
+      let currDay = parseInt(date[2]);
+
+      for (let i = 1; i < currDay + 1; i++) {
+        let day = i < 10 ? `0${i}` : i;
+        let specificDay =
+          "&date=" + date[3] + "-0" + months.indexOf(date[1]) + "-" + day;
+        const root = `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY${specificDay}`
+        this.$http.get(root).then(result => {
+          this.archives = [...this.archives, result.data];
+        });
+      }
+    },
+    toggleArchive: function(e) {
+      e.preventDefault();
+      this.showArchives = !this.showArchives;
     }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #567da5;
   margin-top: 60px;
+}
+body {
+  background-color: rgb(36, 36, 36);
 }
 </style>
